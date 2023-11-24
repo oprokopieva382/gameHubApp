d5423a1f12f56f839124f3644488379c4b09702d
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-11-24 11:47:41 -0600

    create responsive layout for app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
-import Button from "@mui/material/Button";
+import { Box, Grid, Paper, useMediaQuery, useTheme } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 function App() {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
   return (
     <>
-        <Button variant="contained">Contained</Button>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Item>Nav</Item>
+          </Grid>
+
+          {!isMobile ? (
+            <>
+              <Grid item xs={4}>
+                <Item>Aside</Item>
+              </Grid>
+              <Grid item xs={8}>
+                <Item>Main</Item>
+              </Grid>
+            </>
+          ) : (
+            <Grid item xs={12}>
+              <Item>Main</Item>
+            </Grid>
+          )}
+        </Grid>
+      </Box>
     </>
   );
 }
