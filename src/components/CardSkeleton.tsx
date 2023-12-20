d1732dd6895713fdf8a64d54220a1b1160bd3664
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-12-20 00:03:57 -0600

    update skeletons layout

--- src/components/CardSkeleton.tsx
+++ src/components/CardSkeleton.tsx
@@ -2,33 +2,27 @@ import {
   Box,
   Card,
   CardContent,
-  CardMedia,
   Skeleton,
   Typography,
   useTheme,
 } from "@mui/material";
 
-
 export const CardSkeleton = () => {
-   const theme = useTheme();
+  const theme = useTheme();
   return (
-    <>
-      <Card sx={{ maxWidth: 300, boxShadow: theme.shadows[6] }}>
-        <Skeleton />
-        <CardMedia sx={{ height: 160 }} />
-        <Skeleton variant="rounded" width={400}  />
-        <CardContent>
-          <Typography gutterBottom variant="h6" component="div">
-            <Skeleton variant="text" />
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            <Skeleton variant="text" />
-          </Typography>
-        </CardContent>
-        <Box sx={{ display: "flex", justifyContent: "space-between", p: 2 }}>
+    <Card sx={{ width: 300, boxShadow: theme.shadows[6] }}>
+      <Skeleton variant="rectangular" height={160} width={300}/>
+      <CardContent sx={{ height: 100 }}>
+        <Typography gutterBottom variant="h6" component="div">
           <Skeleton variant="text" />
-        </Box>
-      </Card>
-    </>
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          <Skeleton variant="text" />
+        </Typography>
+      </CardContent>
+      <Box sx={{ display: "flex", justifyContent: "space-between", p: 2 }}>
+        <Skeleton variant="text" />
+      </Box>
+    </Card>
   );
 };
