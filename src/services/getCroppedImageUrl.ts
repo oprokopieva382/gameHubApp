51e9180e73a74d10983c8750264d65ebd92b8eb3
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-12-27 10:08:55 -0600

    handle games without img

--- src/services/getCroppedImageUrl.ts
+++ src/services/getCroppedImageUrl.ts
@@ -1,10 +1,12 @@
- const getCroppedImageUrl = (url: string) => {
-  if(!url) return "";
+import noImage from "../assets/no-image-placeholder.webp";
+
+const getCroppedImageUrl = (url: string) => {
+  if (!url) return noImage;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
-  
+
   const modifiedUrl = url.slice(0, index) + "crop/600/400/" + url.slice(index);
-    return modifiedUrl;
+  return modifiedUrl;
 };
 
 export default getCroppedImageUrl;
