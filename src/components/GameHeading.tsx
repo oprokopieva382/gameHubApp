dd09141ec4e5fb69419e8c849dc9bd9622019885
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-12-28 16:35:16 -0600

    update style for GameHeading

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -8,5 +8,5 @@ interface GameHeadingProps {
 
 export const GameHeading: FC<GameHeadingProps> = ({ gameQuery }) => {
     const heading = `${gameQuery.platform?.name || ""} ${gameQuery.genre?.name || ""} Games`
-  return <Typography variant="h4">{heading}</Typography>;
+  return <Typography variant="h4" style={{textAlign: "center"}}>{heading}</Typography>;
 };
