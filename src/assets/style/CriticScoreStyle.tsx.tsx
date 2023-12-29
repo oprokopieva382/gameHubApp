5760b1e7cb36361c80317fa466e85573be725157
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-12-28 18:22:04 -0600

    refactor in CriticScore

---
+++ src/assets/style/CriticScoreStyle.tsx.tsx
@@ -0,0 +1,13 @@
+import Badge from "@mui/material/Badge";
+import { styled } from "@mui/system";
+
+export const StyledBadge = styled(Badge)`
+  display: flex;
+  justify-content: space-between;
+  padding: 6px;
+  font-size: 14px;
+  font-weight: bold;
+  border-radius: 4px;
+  color: #717789;
+  align-items: center;
+`;
