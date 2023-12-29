11828fc902beca15c37fd89d42c87e2a721f0721
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-12-28 19:06:38 -0600

    refactor style in PlatformIconList

---
+++ src/assets/style/PlatformIconListStyle.tsx
@@ -0,0 +1,13 @@
+import Box from "@mui/material/Box";
+import Icon from "@mui/material/Icon";
+import { styled } from "@mui/system";
+
+export const StyledBox = styled(Box)`
+  display: flex;
+  align-items: center;
+`;
+
+export const StyledIcon = styled(Icon)`
+  color: #717789;
+  margin-right: 5px;
+`;
